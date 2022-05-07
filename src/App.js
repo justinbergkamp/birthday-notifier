28f663e5f8e2c837e8487dde3ba5a127e047c53c
justinbergkamp <justin.bergkamp@gmail.com>
2022-05-07 14:55:49 -0500

    testing username

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       {({ signOut, user }) => (
         <div className="App">
           <p>
-            Hey {user.email}, welcome to my channel, with auth!
+            Hey {user.username}, welcome to my channel, with auth!
           </p>
           <button onClick={signOut}>Sign out</button>
         </div>
